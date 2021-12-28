cb2ebb36e4bc6ff3ccb077649701d85ada7c1cac
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-28 16:04:08 +0530

    useRerender

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function App() {
-    // onMouseMove
-    const [mouse, setMouse] = useState("");
+    //useRerender
+    const test = useRef(1);
+    // {Current : 1}
+    const [d, setD] = useState(1);
+
+    console.log("Rerender",test)
     return (
-        <div 
-            onMouseMove={(e) => {
-                console.log(e.clientX, e.clientY);
-                setMouse(`X: ${e.clientX} - Y:${e.clientY}`);
+        <div className="App">
+            <button 
+            onClick={() => {
+                test.current = test.current + 1;
+                console.log(test);
             }}
-      className="App"
-      style={{
-          display: "flex",
-          background: "coral",
-          width: "300px",
-          height: "300px",
-      }}
-      >
-          {mouse}
+            >
+                Increment Test
+            </button>
+
+            <button
+                onClick={() => {
+                    // Force re-rendering
+                    setD((p) => p + 1);
+                }}
+            >
+                Rerender
+            </button>
       </div>
     );
+
+
+
+
+    // // onMouseMove
+    // const [mouse, setMouse] = useState("");
+    // return (
+    //     <div 
+    //         onMouseMove={(e) => {
+    //             console.log(e.clientX, e.clientY);
+    //             setMouse(`X: ${e.clientX} - Y:${e.clientY}`);
+    //         }}
+    //   className="App"
+    //   style={{
+    //       display: "flex",
+    //       background: "coral",
+    //       width: "300px",
+    //       height: "300px",
+    //   }}
+    //   >
+    //       {mouse}
+    //   </div>
+    // );
 }
 
 export default App;
